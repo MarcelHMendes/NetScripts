9b437eba41ffc5c01331958b76caebbc8379a26e
Marcel Mendes <marcelmendes@ufmg.br>
2021-02-18 16:11:01 -0300

    add net_logger

---
+++ c++/base64.h
@@ -0,0 +1,14 @@
+/***************************************************************************************
+*    Title: base64 encoding
+*    Author: Unknown
+*    Date: Oct 1, 2011
+*    Availability: http://www.cplusplus.com/forum/beginner/51572/
+*
+***************************************************************************************/
+
+#ifndef _BASE64_H_
+#define _BASE64_H_
+#include <string>
+std::string base64_encode(unsigned char const* , unsigned int len);
+std::string base64_decode(std::string const& s);
+#endif 
